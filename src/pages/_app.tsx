8819274b9608000946d001ba7ea04cfda16648f0
chiraz-Ag <medjdoubchiraz@gmail.com>
2025-03-03 20:59:39 +0000

    Initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from 'next/app';
+import type { Session } from 'next-auth';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps }
+}: AppProps & { pageProps: { session?: Session } }) {  
+
+  return (
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+  );
 }
